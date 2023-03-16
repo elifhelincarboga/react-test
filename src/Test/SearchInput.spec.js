972e152baaaae444139c2e7780cce985669db121
elifhelincarboga <elifhelincarboga@gmail.com>
2023-03-16 20:52:40 +0300

    test

---
+++ src/Test/SearchInput.spec.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event';
+import App from '../App'
+
+describe('Search Input test', () => { 
+  let searchInput;
+  beforeEach(()=>{
+    render(<App/>)
+    searchInput = screen.getByTestId("searchInput")
+  })
+  test("render after filter",() =>{
+    const value = "Grinning"
+    userEvent.type(searchInput,value)
+    expect(screen.getByText(value)).toBeInTheDocument();
+  })
+})
